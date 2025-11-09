42fec79a6676343f0d63b8f25c252a4076a74204
Samuel Renault <samoulette@asterion.home>
2025-11-09 19:57:12 +0100

    adaptation du code au cas étudié

--- code_TER/solv_lapack_direct.f90
+++ code_TER/solv_lapack_direct.f90
@@ -1,29 +1,18 @@
 program inverse_matrix
     implicit none
-    integer, parameter :: PR = 8, N = 1500 ! limite liée au temps de calcul mais meilleur que piv_gauss
-    real(PR) :: borne_a, borne_b, h, xa, xb
+    integer, parameter :: PR = 8, N = 100 ! limite liée au temps de calcul mais meilleur que piv_gauss
+    real(PR) :: borne_a, borne_b, h
     integer :: info, lda, lwork, ipiv(N), i
     real(PR), dimension(N,N) :: A
     real(PR), dimension(3*N) :: work
     real(PR), dimension(N) :: x, b
 
-    ! intialisation des bornes, du pas de discrétisation et des CL (nul ici sinon jsp comment ça marche)
+    ! intialisation des bornes, du pas de discrétisation
     borne_a = 0
-    xa = 0
     borne_b = 1
-    xb = 0
-    h = (borne_b-borne_a)/(N+1)
+    h = (borne_b-borne_a)/(N-1)
     
-    ! Matrice à inverser
-    A(1,1) = -2
-    A(1,2) = 1
-    do i = 2,N-1
-        A(i,i-1) = 1
-        A(i,i) = -2
-        A(i,i+1) = 1
-    end do
-    A(N,N-1) = 1
-    A(N,N) = -2
+    call init_A_b_2(A, b, h)
 
     lda = N
     lwork = 3 * N
@@ -42,15 +31,9 @@ program inverse_matrix
         stop
     end if
 
-    ! Affichage de la matrice inversée
-    ! print *, "la matrice inverse de A est :"
-    ! call aff_matrix(A, N)
-
-    b = 2
-
     x = MATMUL(A, b)
 
-    call write_in_file("../doc/res_solv_lapack.txt", x, N, h, borne_a, borne_b, xa, xb)
+    call write_in_file("../doc/res_solv_lapack.txt", x, N, h, borne_a, borne_b)
 
 contains
 
@@ -64,20 +47,89 @@ contains
     end subroutine aff_matrix
 
     ! écrit les valeurs utiles dans un fichier texte pour pouvoir les exploiter avec gnuplot (tracé de courbe)
-    subroutine write_in_file(file, x, N, h, a, b, xa, xb)
+    subroutine write_in_file(file, x, N, h, a, b)
         character(len=*), intent(in) :: file
         real(PR), dimension(:), intent(in) ::  x
         integer, intent(in) :: N
-        real(PR), intent(in) :: h, a, b, xa, xb
+        real(PR), intent(in) :: h, a, b
 
         open(unit = 1, file = file, action = "write")
         write (1, '(A)') "#abscisse               ordonnée"
-        write (1, *) a , xa
         do i = 1,N
-        write (1, *) a + (i)*(h), x(i)*h**2
+        write (1, *) a + (i-1)*(h), x(i)
         end do
-        write (1, *) b , xb
         close(1)
     end subroutine write_in_file
+
+    subroutine init_A_b_1(A, b, h)
+        real(PR), dimension(N,N), intent(out) :: A
+        real(PR), dimension(N),intent(out) :: b
+        real(PR), intent(in) :: h
+        integer :: i
+
+        A = 0
+
+        A(1,1) = 1
+        do i = 2,N-1
+            A(i,i-1) = 1
+            A(i,i) = -2
+            A(i,i+1) = 1
+        end do
+        A(N,N) = 1
+
+        ! CL
+        b = 2*h**2
+        b(1) = 0 !u(a)
+        b(N) = 0 !u(b)
+
+    end subroutine init_A_b_1
+
+    subroutine init_A_b_2(A, b, h)
+        real(PR), dimension(N,N), intent(out) :: A
+        real(PR), dimension(N),intent(out) :: b
+        real(PR), intent(in) :: h
+        integer :: k
+        real(PR) :: F, E, I
+
+        E = 1
+        F = -1
+        I = 1
+
+        A = 0
+
+        ! Conditions pour les 2 premières lignes (encastrement) 
+        A(1,1) = 1
+
+        A(2,1) = -1
+        A(2,2) =  1
+
+        ! Conditions pour les 2 dernières lignes de matrice
+        A(N-1, N-2) =  1
+        A(N-1, N-1) = -2
+        A(N-1, N)   =  1
+
+        A(N, N-3) = -1
+        A(N, N-2) =  3
+        A(N, N-1) = -3
+        A(N, N)   =  1
+
+        ! Reste de la matrice A
+
+        do k=3, N-2
+            A(k,k-2) =  1
+            A(k,k-1) = -4
+            A(k,k)   =  6
+            A(k,k+1) = -4
+            A(k,k+2) =  1
+        end do
+
+        ! CL
+        b = 0*h**4
+        b(1) = 0 !u(a)
+        b(2) = 0*h !u'(a)
+        b(N-1) = 0*h**2 !u''(b)
+        b(N) = -F*h**3/(E*I) !u'''(b)
+    end subroutine init_A_b_2
+
 
 end program inverse_matrix
