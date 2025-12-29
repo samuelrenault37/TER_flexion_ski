3d56bdf0ed279ea92b01526afc682df0b362f5a2
Samuel Renault <mmattssam@orion.home>
2025-12-29 19:58:26 +0100

    les paramètres physiques sont passés dans le module constantes

--- code_TER/constantes.f90
+++ code_TER/constantes.f90
@@ -2,4 +2,9 @@ module const
     implicit none
     integer, parameter :: PR = 8
     real(PR), parameter :: epsilon = 1.d-10
+    real(PR), parameter :: F = 245.25_PR ! en N
+    real(PR), parameter :: E = 10d9 ! en Pa
+    real(PR), parameter :: I = 1.44d-8 ! en m**4
+    real(PR), parameter :: borne_a = 0, borne_b = 1.217_PR ! en m
+    integer, parameter :: N_sol = 1000 !pas de discretisation pour tracer la solution analytique (pas de raison de la modifier)
 end module const
