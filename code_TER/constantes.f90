bfff53c4c8f5a675c5a3ab79675d6879ba919286
samuelrenault37 <samuelrenault37@gmail.com>
2026-01-08 20:31:19 +0100

    bonne valeur de F pour exp CRMI

--- code_TER/constantes.f90
+++ code_TER/constantes.f90
@@ -2,7 +2,7 @@ module const
     implicit none
     integer, parameter :: PR = 8
     real(PR), parameter :: epsilon = 1.d-10
-    real(PR), parameter :: F = 245.25_PR ! en N
+    real(PR), parameter :: F = 157_PR ! en N
     real(PR), parameter :: E = 10d9 ! en Pa
     real(PR), parameter :: I = 1.44d-8 ! en m**4
     real(PR), parameter :: borne_a = 0, borne_b = 1.217_PR ! en m
