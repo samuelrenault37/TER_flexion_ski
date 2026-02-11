f062efacd3034e54be13f2f5f29c8100528b2fd1
noahruchdlandr <noah.ruchaud17@gmail.com>
2026-02-11 13:16:34 +0100

    Add constantes module for constants definition

---
+++ code_TER/Poisson/mod_constantes.f90
@@ -0,0 +1,9 @@
+module constantes
+
+    implicit none 
+
+    integer, parameter  :: PR = 8
+    real(PR), parameter :: PI = 4*ATAN(1._PR)
+    real(PR), parameter :: epsilon1 = 1.e-12
+
+end module constantes
