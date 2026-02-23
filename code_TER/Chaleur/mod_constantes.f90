3bfdb1c6ef11b4f8b882d951175af70a8cde8f43
noahruchdlandr <noah.ruchaud17@gmail.com>
2026-02-23 22:42:55 +0100

    Add constantes module with physical constants

---
+++ code_TER/Chaleur/mod_constantes.f90
@@ -0,0 +1,10 @@
+module constantes
+
+    implicit none 
+
+    integer, parameter  :: PR = 8
+    real(PR), parameter :: epsilon1 = 1.e-12
+    real(PR), parameter :: PI = 4*ATAN(1._PR)
+    real(PR), parameter  :: D = 0.5_PR
+
+end module constantes
